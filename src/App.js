1551f1de8e6f928b4c821b3415f0e74c3286ea08
vibhamaurya011 <vibhamaurya011@gmail.com>
2024-03-20 19:42:59 +0530

    basic setup  done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './Layout/Footer';
+import Header from './Layout/Header';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Board/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Column.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Card'
+
+function Column() {
+  return (
+    <section>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </section>
+  )
+}
+
+export default Column
