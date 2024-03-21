3f532c05fd341c6158857e2f3705b45e59fa7207
vibhamaurya011 <vibhamaurya011@gmail.com>
2024-03-21 18:24:42 +0530

    update done

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
-
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -11,7 +10,6 @@ server.use(
  })
 );
 server.use(router);
-// Listen to port
 server.listen(3000, () => {
  console.log("JSON Server is running");
 });
